6c73ff7ceb46332199c8dcbb5ec322d8dbd30608
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2022-03-25 01:22:00 +0300

    first commit

---
+++ src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {increaseByTwoCounter} from '../redux/action/counterActions'
+
+class IncreaseByTwoCounter extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={e => {
+          this
+            .props
+            .dispatch(increaseByTwoCounter());
+        }}>
+          2 arttır
+        </button>
+      </div>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(increaseByTwoCounter, dispatch)
+  }
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter)
